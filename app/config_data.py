5197b2763ad2373110cae42c8eb15ec7ff8d05ec
Luiz <dgjheuh@gmail.com>
2025-08-05 00:37:40 -0300

    Mensagem descrevendo a mudança, ex: Corrige bug do botão

--- app/config_data.py
+++ app/config_data.py
@@ -1,56 +1,64 @@
-# app/config_data.py
-
-# --- E-MAILS DE TESTE PARA PERFIS DE USUÁRIO ---
-# Usados durante o desenvolvimento para simular os diferentes níveis de acesso.
+# Listas de e-mails para atribuição inicial de perfis
 EMAILS_COORDENADORES = [
-    'fgojp61@gmail.com'
+    'fgojp61@gmail.com',
+    'luizedu0494@gmail.com'
 ]
 
 EMAILS_TECNICOS = [
     'dgjheuh@gmail.com'
 ]
 
-
-# --- LISTA OFICIAL DE LABORATÓRIOS ---
+# Lista de Laboratórios
 LISTA_LABORATORIOS = [
-    {'id': 'anatomia_1', 'name': 'Anatomia 1', 'tipo': 'anatomia'},
-    {'id': 'anatomia_2', 'name': 'Anatomia 2', 'tipo': 'anatomia'},
-    {'id': 'anatomia_3', 'name': 'Anatomia 3', 'tipo': 'anatomia'},
-    {'id': 'anatomia_4', 'name': 'Anatomia 4', 'tipo': 'anatomia'},
-    {'id': 'anatomia_5', 'name': 'Anatomia 5', 'tipo': 'anatomia'},
-    {'id': 'anatomia_6', 'name': 'Anatomia 6', 'tipo': 'anatomia'},
-    {'id': 'microscopia_1', 'name': 'Microscopia 1', 'tipo': 'microscopia_normal'},
-    {'id': 'microscopia_2', 'name': 'Microscopia 2', 'tipo': 'microscopia_normal'},
-    {'id': 'microscopia_3', 'name': 'Microscopia 3', 'tipo': 'microscopia_normal'},
-    {'id': 'microscopia_4', 'name': 'Microscopia 4', 'tipo': 'microscopia_normal'},
-    {'id': 'microscopia_5', 'name': 'Microscopia 5', 'tipo': 'microscopia_normal'},
-    {'id': 'microscopia_6_galeria', 'name': 'Microscopia 6 (Galeria)', 'tipo': 'microscopia_galeria'},
-    {'id': 'microscopia_7_galeria', 'name': 'Microscopia 7 (Galeria)', 'tipo': 'microscopia_galeria'},
-    {'id': 'multidisciplinar_1', 'name': 'Multidisciplinar 1', 'tipo': 'multidisciplinar'},
-    {'id': 'multidisciplinar_2', 'name': 'Multidisciplinar 2', 'tipo': 'multidisciplinar'},
-    {'id': 'multidisciplinar_3', 'name': 'Multidisciplinar 3', 'tipo': 'multidisciplinar'},
-    {'id': 'multidisciplinar_4', 'name': 'Multidisciplinar 4', 'tipo': 'multidisciplinar'},
-    {'id': 'habilidade_1_ney_braga', 'name': 'Habilidade 1 (Ney Braga)', 'tipo': 'habilidade_ney_braga'},
-    {'id': 'habilidade_2_ney_braga', 'name': 'Habilidade 2 (Ney Braga)', 'tipo': 'habilidade_ney_braga'},
-    {'id': 'habilidade_3_ney_braga', 'name': 'Habilidade 3 (Ney Braga)', 'tipo': 'habilidade_ney_braga'},
-    {'id': 'habilidades_1_santander', 'name': 'Habilidades 1 (Santander)', 'tipo': 'habilidade_santander'},
-    {'id': 'habilidades_2_santander', 'name': 'Habilidades 2 (Santander)', 'tipo': 'habilidade_santander'},
-    {'id': 'habilidades_3_santander', 'name': 'Habilidades 3 (Santander)', 'tipo': 'habilidade_santander'},
-    {'id': 'habilidades_1_galeria', 'name': 'Habilidades 1 (Galeria)', 'tipo': 'habilidade_galeria'},
-    {'id': 'habilidades_2_galeria', 'name': 'Habilidades 2 (Galeria)', 'tipo': 'habilidade_galeria'},
-    {'id': 'habilidades_3_galeria', 'name': 'Habilidades 3 (Galeria)', 'tipo': 'habilidade_galeria'},
-    {'id': 'farmaceutico', 'name': 'Farmacêutico', 'tipo': 'farmaceutico'},
-    {'id': 'tec_dietetica', 'name': 'Tec. Dietética', 'tipo': 'tecdietetica'},
-    {'id': 'uda', 'name': 'UDA', 'tipo': 'uda'}
+    # Anatomia
+    {'id': 'anatomia_1', 'name': 'Anatomia 1'},
+    {'id': 'anatomia_2', 'name': 'Anatomia 2'},
+    {'id': 'anatomia_3', 'name': 'Anatomia 3'},
+    {'id': 'anatomia_4', 'name': 'Anatomia 4'},
+    {'id': 'anatomia_5', 'name': 'Anatomia 5'},
+    {'id': 'anatomia_6', 'name': 'Anatomia 6'},
+    # Microscopia
+    {'id': 'microscopia_1', 'name': 'Microscopia 1'},
+    {'id': 'microscopia_2', 'name': 'Microscopia 2'},
+    {'id': 'microscopia_3', 'name': 'Microscopia 3'},
+    {'id': 'microscopia_4', 'name': 'Microscopia 4'},
+    {'id': 'microscopia_5', 'name': 'Microscopia 5'},
+    {'id': 'microscopia_6_galeria', 'name': 'Microscopia 6 (Galeria)'},
+    {'id': 'microscopia_7_galeria', 'name': 'Microscopia 7 (Galeria)'},
+    # Multidisciplinar
+    {'id': 'multidisciplinar_1', 'name': 'Multidisciplinar 1'},
+    {'id': 'multidisciplinar_2', 'name': 'Multidisciplinar 2'},
+    {'id': 'multidisciplinar_3', 'name': 'Multidisciplinar 3'},
+    {'id': 'multidisciplinar_4', 'name': 'Multidisciplinar 4'},
+    # Habilidades Ney Braga (ATUALIZADO)
+    {'id': 'habilidade_1_ney', 'name': 'Habilidade 1 (baixa complexidade) (Ney Braga)'},
+    {'id': 'habilidade_2_ney', 'name': 'Habilidade 2 (media complexidade) (Ney Braga)'},
+    {'id': 'habilidade_3_ney', 'name': 'Habilidade 3 (media complexidade) (Ney Braga)'},
+    {'id': 'habilidade_4_ney', 'name': 'Habilidade 4 (alta complexidade) (Ney Braga)'},
+    # Habilidades Santander (Inalterado)
+    {'id': 'habilidades_1_santander', 'name': 'Habilidades 1 (Santander)'},
+    {'id': 'habilidades_2_santander', 'name': 'Habilidades 2 (Santander)'},
+    {'id': 'habilidades_3_santander', 'name': 'Habilidades 3 (Santander)'},
+    # Habilidades Galeria (Inalterado)
+    {'id': 'habilidades_1_galeria', 'name': 'Habilidades 1 (Galeria)'},
+    {'id': 'habilidades_2_galeria', 'name': 'Habilidades 2 (Galeria)'},
+    {'id': 'habilidades_3_galeria', 'name': 'Habilidades 3 (Galeria)'},
+    # Outros
+    {'id': 'farmaceutico', 'name': 'Farmacêutico'},
+    {'id': 'tec_dietetica', 'name': 'Tec. Dietética'},
+    {'id': 'uda', 'name': 'UDA'},
 ]
 
-
-# --- BLOCOS DE HORÁRIO PARA AGENDAMENTO ---
+# Blocos de Horário
 BLOCOS_HORARIO = [
-    {"value": "07:00-09:10", "label": "07:00 - 09:10", "turno": "Matutino"},
-    {"value": "09:30-12:00", "label": "09:30 - 12:00", "turno": "Matutino"},
-    {"value": "13:00-15:10", "label": "13:00 - 15:10", "turno": "Vespertino"},
-    {"value": "15:30-18:00", "label": "15:30 - 18:00", "turno": "Vespertino"},
-    {"value": "18:30-20:10", "label": "18:30 - 20:10", "turno": "Noturno"},
-    {"value": "20:30-22:00", "label": "20:30 - 22:00", "turno": "Noturno"},
+    # Manhã
+    {'value': '07:00 - 09:10', 'label': '07:00 - 09:10', 'turno': 'Manhã'},
+    {'value': '09:20 - 11:30', 'label': '09:20 - 11:30', 'turno': 'Manhã'},
+    {'value': '11:40 - 12:30', 'label': '11:40 - 12:30', 'turno': 'Manhã'},
+    # Tarde
+    {'value': '13:30 - 15:40', 'label': '13:30 - 15:40', 'turno': 'Tarde'},
+    {'value': '15:50 - 18:00', 'label': '15:50 - 18:00', 'turno': 'Tarde'},
+    # Noite
+    {'value': '18:30 - 20:40', 'label': '18:30 - 20:40', 'turno': 'Noite'},
+    {'value': '20:50 - 22:00', 'label': '20:50 - 22:00', 'turno': 'Noite'},
 ]
